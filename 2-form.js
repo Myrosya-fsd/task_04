3cb1340dc3d41a3c1d19d6d5e3a9cec13b9b8b7c
Myrosya-fsd <murosja80@gmail.com>
2025-04-06 19:11:36 +0000

    Deploying to gh-pages from @ Myrosya-fsd/task_04@ca7891c129d32efda7e31bdb31d49e0b6c02efc1 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{A as o}from"./assets/vendor-BQPQti7Y.js";const t=document.querySelector(".container-list");new o(t,{showMultiple:!0,duration:400});
+//# sourceMappingURL=2-form.js.map
