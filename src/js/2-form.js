bf06b5d62ac97d2bef73ebba23889d9368e17d34
myroslava <murosja80@gmail.com>
2025-04-06 22:15:46 +0300

    task_04

--- src/js/2-form.js
+++
@@ -1,8 +0,0 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
-const container = document.querySelector('.container-list');
-const accordion = new Accordion(container, {
-  showMultiple: true,
-  duration: 400,
-});
