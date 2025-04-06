174b716ba301889f8f2700ec94da6e2d5994e3a7
Myrosya-fsd <murosja80@gmail.com>
2025-04-06 19:16:13 +0000

    Deploying to gh-pages from @ Myrosya-fsd/task_04@bf06b5d62ac97d2bef73ebba23889d9368e17d34 🚀

--- assets/4-task-B2x_wNrU.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper-2",{slidesPerView:1,spaceBetween:20,breakpointBase:window,navigation:{nextEl:".swiper-2 .swiper-button-next",prevEl:".swiper-2 .swiper-button-prev"},keyboard:{enabled:!0,onlyInViewport:!0},mousewheel:!0});
-//# sourceMappingURL=4-task-B2x_wNrU.js.map
